429b8a4669ebd56f649b2e47233b4968d14a78f4
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-07-25 00:43:07 +0300

    add all

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import { FcSearch } from 'react-icons/fc';
+import {
+  Header,
+  SearchForm,
+  SearchFormInput,
+  SearchButton,
+  SearchButtonLabel,
+} from './Searchbar.styled';
+
+const Searchbar = ({ onSubmit }) => {
+  return (
+    <Header>
+      <Formik
+        initialValues={{ search: '' }}
+        onSubmit={({ search }) => onSubmit(search)}
+      >
+        <SearchForm>
+          <SearchButton type="submit">
+            <FcSearch size={24} />
+            <SearchButtonLabel>Search</SearchButtonLabel>
+          </SearchButton>
+          <SearchFormInput
+            type="text"
+            name="search"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </SearchForm>
+      </Formik>
+    </Header>
+  );
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Searchbar;
